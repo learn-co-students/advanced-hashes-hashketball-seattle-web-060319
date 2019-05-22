fa6ff0f14062490e3598b0f3f6b70a66d8690fe8
Michael Maggs <michaelmaggs@gmail.com>
2019-05-22 20:11:10 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -4,7 +4,7 @@ def game_hash
   :home => 
     {:team_name => "Brooklyn Nets", :colors => ["Black", "White"], :players =>
       {
-        {"Alan Anderson" =>
+        "Alan Anderson" =>
          { number: "0", 
            shoe: "16",
            points: "22",
@@ -13,8 +13,8 @@ def game_hash
            steals: "3",
            blocks: "1",
            slam_dunks:"1"
-          }},
-        {"Reggie Evans" =>
+          },
+        "Reggie Evans" =>
           { number: "30",
             shoe: "14",
             points: "12",
@@ -23,8 +23,8 @@ def game_hash
             steals: "12",
             blocks: "12",
             slam_dunks: "7"
-          }},
-        {"Brook Lopez" => 
+          },
+        "Brook Lopez" => 
           { number: "11",
             shoe: "17",
             points: "17",
@@ -33,8 +33,8 @@ def game_hash
             steals: "3",
             blocks: "1",
             slam_dunks: "15"
-          }},
-        {"Mason Plumlee" => 
+          },
+        "Mason Plumlee" => 
           { number: "1",
             shoe: "19",
             points: "26",
@@ -43,8 +43,8 @@ def game_hash
             steals: "3",
             blocks: "8",
             slam_dunks: "5"
-          }},
-        {"Jason Terry" => 
+          },
+        "Jason Terry" => 
           { number:"31",
             shoe: "15",
             points: "19",
@@ -53,11 +53,12 @@ def game_hash
             steals: "4",
             blocks: "11",
             slam_dunks: "1"
-          }}}
+          }
+      }
     },
 :away => 
     {:team_name => "Charlotte Hornets", :colors => ["Turquoise", "Purple"],:players => {
-        {"Jeff Adrien" => 
+        "Jeff Adrien" => 
           { number: "4",
             shoe: "18",
             points: "10",
@@ -66,8 +67,8 @@ def game_hash
             steals: "2",
             blocks: "7",
             slam_dunks: "2"
-          }},
-        {"Bismack Biyombo" => 
+          },
+        "Bismack Biyombo" => 
           { number: "0",
             shoe: "16",
             points: "12",
@@ -76,8 +77,8 @@ def game_hash
             steals: "7",
             blocks: "15",
             slam_dunks: "10"
-          }},
-        {"DeSagna Diop" => 
+          },
+        "DeSagna Diop" => 
           { number: "2",
             shoe: "14",
             points: "24",
@@ -86,8 +87,8 @@ def game_hash
             steals: "4",
             blocks: "5",
             slam_dunks: "5"
-          }},
-        {"Ben Gordon" => 
+          },
+        "Ben Gordon" => 
           { number: "8",
             shoe: "15",
             points: "33",
@@ -96,8 +97,8 @@ def game_hash
             steals:"1",
             blocks: "1",
             slam_dunks: "0"
-          }},
-        {"Brendan Haywood" =>
+          },
+        "Brendan Haywood" =>
           { number: "33",
             shoe: "15",
             points: "6",
@@ -106,22 +107,26 @@ def game_hash
             steals: "22",
             blocks: "5",
             slam_dunks: "12"
-          }}}
+          }
     }
+}
 }
 end
 
 
 def num_points_scored(name)
   game_hash.each do |location, attributes|
-    location[:players].each do |stats, info|
-       binding.pry
-      if name == stats
-        return [:players][points]
-      end
-    end
-  end
-end
+    attributes.each do |attribute|
+      if attribute == :players
+       attribute.each do |players, stats|
+        if players = name
+          return players[points]
+        end
+       end
+     end
+   end
+ end
+ end
 
 
 
