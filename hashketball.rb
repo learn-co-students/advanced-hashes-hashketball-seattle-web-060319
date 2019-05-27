9d2a1339016aaed18afc16adb829ee0755bc6f37
Kara O <karajo.odle@gmail.com>
2019-05-27 19:28:42 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,9 +1,83 @@
-# Write your code here!
+def game_hash
+  {
+    :home => {
+      :team_name => "Brooklyn Nets",
+      :colors => ["Black", "White"],
+      :players => {
+        "Alan Anderson" => {:number => 0, :shoe => 16, :points => 22, :rebounds => 12, :assists => 12, :steals => 3, :blocks => 1, :slam_dunks => 1},
+        "Reggie Evans" => {:number => 30, :shoe => 14, :points => 12, :rebounds => 12, :assists => 12, :steals => 12, :blocks => 12, :slam_dunks => 7},
+        "Brook Lopez" => {:number => 11, :shoe => 17, :points => 17, :rebounds => 19, :assists => 10, :steals => 3, :blocks => 1, :slam_dunks => 15},
+        "Mason Plumlee" => {:number => 1, :shoe => 19, :points => 26, :rebounds => 12, :assists => 6, :steals => 3, :blocks => 8, :slam_dunks => 5},
+        "Jason Terry" => {:number => 31, :shoe => 15, :points => 19, :rebounds => 2, :assists => 2, :steals => 4, :blocks => 11, :slam_dunks => 1},
+      }
+    },
+    :away => {
+      :team_name => "Charlotte Hornets",
+      :colors => ["Turquoise", "Purple"],
+      :players => {
+        "Jeff Adrien" => {:number => 4, :shoe => 18, :points => 10, :rebounds => 1, :assists => 1, :steals => 2, :blocks => 7, :slam_dunks => 2},
+        "Bismack Biyombo" => {:number => 0, :shoe => 16, :points => 12, :rebounds => 4, :assists => 7, :steals => 7, :blocks => 15, :slam_dunks => 10},
+        "DeSagna Diop" => {:number => 2, :shoe => 14, :points => 24, :rebounds => 12, :assists => 12, :steals => 4, :blocks => 5, :slam_dunks => 5},
+        "Ben Gordon" => {:number => 8, :shoe => 15, :points => 33, :rebounds => 3, :assists => 2, :steals => 1, :blocks => 1, :slam_dunks => 0},
+        "Brendan Haywood" => {:number => 33, :shoe => 15, :points => 6, :rebounds => 12, :assists => 12, :steals => 22, :blocks => 5, :slam_dunks => 12},
+      }
+    },
+  }
+end
 
+def num_points_scored(player_name)
+  game_hash[:home][:players].include?(player_name) ? game_hash[:home][:players][player_name][:points] : game_hash[:away][:players][player_name][:points]
+end
 
+def shoe_size(player_name)
+  game_hash[:home][:players].include?(player_name) ? game_hash[:home][:players][player_name][:shoe] : game_hash[:away][:players][player_name][:shoe]
+end
 
+def team_colors(team_name)
+  game_hash[:home][:team_name] == team_name ? game_hash[:home][:colors] : game_hash[:away][:colors]
+end
 
+def team_names
+  team_array = []
+  team_array << game_hash[:home][:team_name]
+  team_array << game_hash[:away][:team_name]
+  team_array
+end
+  
+def player_numbers(team_name)
 
+  if game_hash[:home][:team_name] == team_name
+    game_hash[:home][:players].collect do |name, stats|
+      game_hash[:home][:players][name][:number]
+    end
+
+  else
+    game_hash[:away][:players].collect do |name, stats|
+      game_hash[:away][:players][name][:number]
+    end
+  end
+end
+
+def player_stats(player_name)
+  game_hash[:home][:players].include?(player_name) ? game_hash[:home][:players][player_name] : game_hash[:away][:players][player_name]
+end
+
+def big_shoe_rebounds
+  hash1 = game_hash[:away][:players]
+  hash2 = game_hash[:home][:players]
+
+  final_hash = hash1.merge(hash2) 
+  
+  largest_shoe = nil
+  largest_shoe_rebounds = nil 
+  final_hash.each do |name, stats|
+    if largest_shoe == nil || final_hash[name][:shoe] > largest_shoe
+      largest_shoe = final_hash[name][:shoe]
+      largest_shoe_rebounds = final_hash[name][:rebounds]
+    end
+  end
+largest_shoe_rebounds
+end
 
 
 
